1d8dd4b998d053f94432c08af46d3be7c94c5324
Gabriel Figueroa <halo200567@gmail.com>
2025-07-08 14:16:50 -0400

    Add reset button function

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ btn.addEventListener('click', () => {
   greeting.textContent = 'Hello from User 1!';
 });
 
+const resetBtn = document.getElementById('resetBtn');
+resetBtn.addEventListener('click', () => {
+greeting.textContent = 'Welcome!';
+});
